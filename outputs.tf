5fdbd2ddc4ed0fed24ffee640a8f8d34cd0cd6ac
harry.sarshogh <harry.sarshogh@gmail.com>
2026-05-27 00:11:14 +0100

    feat: implement tf-atom-elasticache-replication-group-aws module

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,24 @@
-output "enabled" {
-  description = "Whether the module is enabled."
-  value       = local.enabled
+output "id" {
+  description = "ID of the replication group"
+  value       = try(aws_elasticache_replication_group.this[0].id, null)
+}
+
+output "arn" {
+  description = "ARN of the replication group"
+  value       = try(aws_elasticache_replication_group.this[0].arn, null)
+}
+
+output "primary_endpoint_address" {
+  description = "Primary endpoint address"
+  value       = try(aws_elasticache_replication_group.this[0].primary_endpoint_address, null)
+}
+
+output "reader_endpoint_address" {
+  description = "Reader endpoint address"
+  value       = try(aws_elasticache_replication_group.this[0].reader_endpoint_address, null)
+}
+
+output "port" {
+  description = "Port number"
+  value       = try(aws_elasticache_replication_group.this[0].port, null)
 }

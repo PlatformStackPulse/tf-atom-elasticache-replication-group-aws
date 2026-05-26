7a909a34db07ea8414e4240b61317685d438c4f4
harry.sarshogh <harry.sarshogh@gmail.com>
2026-05-27 00:11:14 +0100

    feat: implement tf-atom-elasticache-replication-group-aws module

--- variables.tf
+++ variables.tf
@@ -1,7 +1,114 @@
-# -----------------------------------------------------------------------------
-# Module-Specific Variables
-#
-# Note: Standard labeling variables (enabled, namespace, tenant, environment,
-# stage, name, delimiter, attributes, tags, label_order, etc.) are provided
-# by context.tf via the tf-label module.
-# -----------------------------------------------------------------------------
+variable "node_type" {
+  description = "ElastiCache node type"
+  type        = string
+  default     = "cache.t3.micro"
+}
+
+variable "num_cache_clusters" {
+  description = "Number of cache clusters (nodes) in the replication group"
+  type        = number
+  default     = 2
+}
+
+variable "port" {
+  description = "Port number"
+  type        = number
+  default     = 6379
+}
+
+variable "parameter_group_name" {
+  description = "ElastiCache parameter group name"
+  type        = string
+  default     = null
+}
+
+variable "subnet_group_name" {
+  description = "ElastiCache subnet group name"
+  type        = string
+  default     = null
+}
+
+variable "security_group_ids" {
+  description = "List of security group IDs"
+  type        = list(string)
+  default     = []
+}
+
+variable "engine_version" {
+  description = "Redis engine version"
+  type        = string
+  default     = "7.1"
+}
+
+variable "automatic_failover_enabled" {
+  description = "Enable automatic failover"
+  type        = bool
+  default     = true
+}
+
+variable "multi_az_enabled" {
+  description = "Enable Multi-AZ"
+  type        = bool
+  default     = true
+}
+
+variable "at_rest_encryption_enabled" {
+  description = "Enable encryption at rest"
+  type        = bool
+  default     = true
+}
+
+variable "transit_encryption_enabled" {
+  description = "Enable encryption in transit"
+  type        = bool
+  default     = true
+}
+
+variable "auth_token" {
+  description = "Auth token for Redis (requires transit_encryption_enabled)"
+  type        = string
+  sensitive   = true
+  default     = null
+}
+
+variable "kms_key_id" {
+  description = "KMS key ID for at-rest encryption"
+  type        = string
+  default     = null
+}
+
+variable "snapshot_retention_limit" {
+  description = "Number of days to retain snapshots"
+  type        = number
+  default     = 7
+}
+
+variable "snapshot_window" {
+  description = "Daily time range for snapshots"
+  type        = string
+  default     = "03:00-05:00"
+}
+
+variable "maintenance_window" {
+  description = "Weekly maintenance window"
+  type        = string
+  default     = "mon:05:00-mon:06:00"
+}
+
+variable "auto_minor_version_upgrade" {
+  description = "Auto minor version upgrade"
+  type        = bool
+  default     = true
+}
+
+variable "apply_immediately" {
+  description = "Apply changes immediately"
+  type        = bool
+  default     = false
+}
+
+variable "description" {
+  description = "Description of the replication group"
+  type        = string
+  default     = null
+}
